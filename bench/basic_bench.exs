38e75f9b7bea5d91ea94ad21e91082369f9ed26e
WangJinzhong <andywang7259@163.com>
2018-06-09 00:26:50 +0800

    bench

--- bench/basic_bench.exs
+++ bench/basic_bench.exs
@@ -4,6 +4,6 @@ defmodule BasicBench do
   @test_string "99da龡龢da龣龤龥癩s什么鬼东西hah"
 
   bench "hello pinyin" do
-    Pinyin.from_string @test_string
+    PinyinUtil.spell @test_string
   end
 end
--- bench/bench.rb
+++ bench/bench.rb
@@ -4,6 +4,6 @@ require 'chinese_pinyin'
 
 n = 500000
 Benchmark.bmbm(7) do |x|
-  x.report("pinyin") { n.times {Pinyin.t("99da龡龢da龣龤龥癩s什么鬼东西hah")} }
+  x.report("pinyin_util") { n.times {PinyinUtil.t("99da龡龢da龣龤龥癩s什么鬼东西hah")} }
 end
 
